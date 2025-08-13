fc5c5e249336446d9722dba60d9946c06732f623
Rutuja Davle <rutujadawale2005@gmail.com>
2025-08-13 23:01:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const swiper = new Swiper(".wrapper", {
+  loop: true,
+  spaceBetween: 30,
+
+  //autoplay
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  // need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
